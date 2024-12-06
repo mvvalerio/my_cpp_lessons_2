0c31257fafa467fef31d10b1de0524dcff62cd1a
mvvalerio <martimvv07@gmail.com>
2024-12-06 12:15:09 -0100

    Done

--- exemplos/advancedexs.cpp
+++ exemplos/advancedexs.cpp
@@ -15,21 +15,26 @@ void graficohorizontal(int x, int y, int z){
     if(y > maior) maior = y;
     if(z > maior) maior = z;
 
+
     int a,b,c;
     a = x;
     b = y;
     c = z;
 
+
     x = x * 50 / maior;
     y = y * 50 / maior;
     z = z * 50 / maior;
 
+
     cout << "x: " << setw(2) << a << " ";
     for(int i = 0; i < x; i++) cout << "*";
     cout << endl;
+
     cout << "y: " << setw(2) << b << " ";
     for(int i = 0; i < y; i++) cout << "*";
     cout << endl;
+
     cout << "z: " << setw(2) << c << " ";
     for(int i = 0; i < z; i++) cout << "*";
     cout << endl;
@@ -41,7 +46,7 @@ void graficohorizontal(int x, int y, int z){
 
 int main(){
 
-    graficohorizontal(100, 20, 900);
+    graficohorizontal(100, 20, 300);
     
     return 0;
 }
