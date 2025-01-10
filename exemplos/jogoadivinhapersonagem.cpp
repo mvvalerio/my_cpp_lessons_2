a4d152f7aca3f25579e7b0e4fa1e44beeafe7205
Valério <145759435+mvvalerio@users.noreply.github.com>
2025-01-10 21:56:56 -0100

    Done

--- exemplos/jogoadivinhapersonagem.cpp
+++ exemplos/jogoadivinhapersonagem.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <ctime>
+#include <cstdlib>
 
 using namespace std;
 
@@ -13,35 +14,75 @@ struct Marvel{
 
 };
 
-
 Marvel escolha;
-
-char cregisto;
-string sregisto;
 
 void joguin(){
 
     string personagem;
+    string entrada;
+    bool acertou = false;
 
     srand(time(NULL));
 
     int gera = rand() % 5;
     if(gera == 0) personagem = escolha.b;
     else if(gera == 1) personagem = escolha.s;
-    else if(gera == 1) personagem = escolha.h;
-    else if(gera == 1) personagem = escolha.i;
+    else if(gera == 2) personagem = escolha.h;
+    else if(gera == 3) personagem = escolha.i;
     else personagem = escolha.w;
 
     string linhas(personagem.size(), '_');
 
-    string entrada;
-    bool acerta = false;
+    while(linhas != personagem){
 
-    while(personagem != linhas){
+        cout << "\nPersonagem: " << linhas << endl;
+        cout << "Coloque uma letra ou o nome completo: ";
+        cin >> entrada;
+        
+        system("clear");
+    
+        if(entrada == personagem){
+        
+            linhas = personagem;
+            break;
+        
+        }
+    
+        if(entrada.size() == 1){
+        
+            char letra = entrada[0];
+            acertou = false;
+    
+            for(size_t i = 0; i < personagem.size(); i++){
+        
+                if(personagem[i] == letra){
+            
+                    linhas[i] = letra;
+                    acertou = true;
+            
+                }
+            }
 
-
-
+            if(!acertou){
+        
+                cout << "\nErrou ! Outra letra" << endl;
+        
+            } else {
+        
+                cout << "Acertou uma letra. Continue assim !" << endl;
+        
+            }
+        
+        } else {
+        
+        cout << "Nome Incorreto !" << endl;
+        
+        }
     }
+    
+    system("clear");
+    
+    cout << "PARABÉNS ! Acertou o nome do personagem: " << personagem << endl;
 
 }
 
